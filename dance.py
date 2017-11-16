fce68e74f8b8e3bb2f2db552e7a9fab3c4f1c88a
nuzen <venkatesh.jatla@gmail.com>
2017-11-16 19:01:39 +0000

    Dance made by Polk girl

--- dance.py
+++ dance.py
@@ -1,6 +1,17 @@
 import aolme
-aolme.rightsec(1)
+#aolme.rightsec(1)
+#aolme.fw(1)
+#aolme.bw(1)
+#aolme.leftsec(1)
+#aolme.resetSensors()
+
+
+aolme.rightsec(1.5)
 aolme.fw(1)
+aolme.leftsec(3)
+aolme.fw(1)
+aolme.rightsec(1.5)
 aolme.bw(1)
-aolme.leftsec(1)
+aolme.fw(1)
+aolme.leftsec(1.5)
 aolme.resetSensors()
